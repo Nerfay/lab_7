db532ae66b03bee1244677d27e22d59da2e373eb
Nerfay <101548076+Nerfay@users.noreply.github.com>
2023-10-27 20:22:52 +0300

    Add files via upload

---
+++ Laba_7_V_1/Product.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+
+namespace Laba_7_V_1
+{
+    public class Product
+    {
+        private double _price;
+        private string _name;
+        private string _brand;
+        private bool _isPromotional;
+        private double _promotionalPrice;
+
+        public Product(double price, string name, string brand, double promotionalPrice=double.NaN)
+        {
+            Price = price;
+            Name = name;
+            Brand = brand;
+            if (!double.IsNaN(promotionalPrice))
+            {
+                IsPromotional = true;
+                PromotionalPrice = promotionalPrice;
+            }
+            else
+            {
+                IsPromotional = false;
+            }
+        }
+
+
+        /// <summary>
+        ///СВойство цена продукта , возвращает ArgumentOutOfRangeException, если пытаемся присвоить отрицательное значение 
+        /// </summary>
+        public double Price
+        {
+            get => _price;
+            init
+            {
+                if (value > 0 )
+                {
+                    _price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        /// <summary>
+        /// Свойство название продукта , возвращает ArgumentNullException, если пытаемся присвоить пустую строку
+        /// </summary>
+        public string Name
+        {
+            get => _name;
+            init
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _name = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+        /// <summary>
+        /// Свойство бренд,фирма продукта , возвращает ArgumentNullException, если пытаемся присвоить пустую строку
+        /// </summary>
+        public string Brand
+        {
+            get => _brand;
+            init
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    _brand = value.Trim();
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+        /// <summary>
+        /// Свойство акционный ли товар 
+        /// </summary>
+        public bool IsPromotional
+        {
+            get => _isPromotional;
+            init => _isPromotional = value;
+        }
+        /// <summary>
+        ///СВойство акционная цена продукта , возвращает ArgumentOutOfRangeException, если пытаемся присвоить отрицательное значение или значение больше обычной цены
+        /// </summary>
+        public double PromotionalPrice
+        {
+            get => _promotionalPrice;
+            init
+            {
+                if (value > 0 && value<Price)
+                {
+                    _promotionalPrice = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+        public override string ToString()
+        {
+            string IspromotionalString = "";
+            if (IsPromotional)
+            {
+                IspromotionalString = $"promotional price= {PromotionalPrice} ;";
+            }
+            return $"Product(name = {Name} ; brand= {Brand} ;price= {Price} ; {IspromotionalString})\n";
+        }
+    }
+}
---
+++ Laba_7_V_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Laba_7_V_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product(price: 10.33, name: "bread", brand: "OMXA",promotionalPrice:9.3);
+            Product product2 = new Product(price: 10.33, name: "dress", brand: "VS", promotionalPrice: 3.3);
+            Product product3 = new Product(price: 100000000000000, name: "car", brand: "OPEL");
+            Product product4 = new Product(price: 700.450, name: "iphone", brand: "Apple", promotionalPrice: 600);
+            Product product5 = new Product(price: 9.99, name: "cup", brand: "My Home", promotionalPrice: 9.3);
+            Product product6 = new Product(price: 6501.33, name: "t-short", brand: "lives");
+
+            ShoppingList shoppingList = new ShoppingList(product3);
+            //Добавление елементов product6,product2,product1,product5
+            shoppingList.AddElement(product6);
+            shoppingList.AddElement(product2);
+            shoppingList.AddElement(product1);
+            shoppingList.AddElement(product5);
+            Console.WriteLine("Shopping list:");
+            Console.WriteLine(shoppingList);
+            //Удаление елемента по индексу 3 
+            shoppingList.RemoveElementByIndex(3);
+            Console.WriteLine("Shopping list:");
+            Console.WriteLine(shoppingList);
+            //Удаление елемента product2 и product5
+            shoppingList.RemoveElementByProduct(product2);
+            shoppingList.RemoveElementByProduct(product5);
+            Console.WriteLine("Shopping list:");
+            Console.WriteLine(shoppingList);
+
+        }
+    }
+}
